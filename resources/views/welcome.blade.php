6514b4bfa6dd577d86500a4a04a5117e3e04ac0d
anasfarooqui7 <67817524+anasfarooqui7@users.noreply.github.com>
2020-09-26 20:13:54 +0530

    my first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.welcome')
+
+@section('content')
+   <div class="container">
+       <div class="row">
+           <div class="col-6 d-felx flex-colum justify-content-center py-5">
+               <div class="py-3"><h1>Link building is one of the most important thing in <span class="text-danger">SEO</span></h1></div>
+               <div ><h4>Every little helps. Submit your domain and get do follow backlink from our website</h4></div>
+               <div class="py-3"><a href="#" class="btn btn-primary">Submit your domain</a></div>
+           </div>
+       </div>
+       <div class="d-flex justify-content-center">
+          <h2 class="font-weight-bolder">Categories</h2>
+       </div>
+   </div>
+@endsection
